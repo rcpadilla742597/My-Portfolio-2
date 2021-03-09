0821eef71e4cbe91d2b442e6483d2c6a61b986fe
rcpadilla742597 <rcpadilla1992@gmail.com>
2021-03-09 12:39:13 -0500

    Added contact form with Firebase

--- app.js
+++ app.js
@@ -13,25 +13,79 @@ async function postData() {
 postData();
 */
 
-/* Use to grab onto Firebase -> Connection
-
+// Your web app's Firebase configuration
 var firebaseConfig = {
     apiKey: "AIzaSyBKF11oVlR327fvNYmlB449yHW4WWRzV6Q",
     authDomain: "contactform-portfolio-65fc3.firebaseapp.com",
+    databaseURL: "https://contactform-portfolio-65fc3-default-rtdb.firebaseio.com",
     projectId: "contactform-portfolio-65fc3",
     storageBucket: "contactform-portfolio-65fc3.appspot.com",
     messagingSenderId: "1098490301086",
     appId: "1:1098490301086:web:2a82c5f8bb6d7329f2c98f"
+  };
 
-// Init Firebase
-firebaseConfig.initializeApp(firebaseConfig());
-var firestore = firebase.firestore();
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
-// Start grabbing our DOM element
-const submitBtn = document.querySelector('#submit') */
+// Reference messages collection in Firebase
+var messagesRef = firebase.database().ref('messages');
+
+// contactForm: Listen for form submit. Getting the ID contactForm and adding an event listener to the 'submit' button when submitForm activates.
+
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Creating a function called submitForm that has an eventListener asssigned to it. When you submit the form, the function saveMessage will save what is inputed into the fields
+
+function submitForm(e){
+    e.preventDefault();
+
+    // Getting the values
+    var fname = getInputVal('fname');
+    var lname = getInputVal('lname');
+    var company = getInputVal('company');
+    var email = getInputVal('email');
+    var phone = getInputVal('phone');
+    var message = getInputVal('message');
+
+    // Save message
+    saveMessage(fname, lname, company, email, phone, message);
+
+    //Show alert when submitting
+    document.querySelector('.alert').style.display = 'block';
+
+    //Hide alert after 3 seconds
+    setTimeout(function(){
+        document.querySelector('.alert').style.display = 'none';
+    }, 3000);
+    
+    // Clears the form
+    document.getElementById('contactForm').reset();
+}
+
+// Creating a function to get form values. I don't have to keep writting document.getElementById for each field.
+
+function getInputVal(id) {
+    return document.getElementById(id).value;
+    // I just want the value that's put into the field.
+}
+
+// Creating a function that sends an object of data to my messages collection in Firebase.
+
+function saveMessage(fname, lname, company, email, phone, message) {
+    var newMessageRef = messagesRef.push();
+    newMessageRef.set({
+        fname: fname,
+        lname:lname,
+        company:company,
+        email:email,
+        phone:phone,
+        message:message
+    });
+}
 
 
 
+// Hamburger menu
 const hamburger = document.querySelector('.hamburger'); /* Matches the first element within the document that matches the specified selector. I am setting hamburger to the querySelector */
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links li'); /* Selects all of the links */
